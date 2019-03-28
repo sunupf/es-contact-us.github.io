ba070d6cf84208214c469c54750bd65b406e6b71
kokok <sunupf@gmail.com>
2019-03-28 18:14:21 +0700

    add loading screen

--- scripts.js
+++ scripts.js
@@ -2,18 +2,43 @@ function toggleDialog() {
 
 }
 
-var button = document.querySelector('a.button');
+function toggleClass() {
+  
+}
+
+function toggleFormPlaceholder() {
+
+}
+
+function getFormLabel(input) {
+  return input.parentElement.querySelector('label')
+}
+
+var openButton = document.querySelector('a.button');
+var closeButton = document.querySelector('.close-modal');
 var inputs = document.querySelectorAll('input, textarea');
+var modalContact = document.querySelector('#contact-us');
 
-button.addEventListener('click', function() {
 
+openButton.addEventListener('click', function() {
+  modalContact.classList.add('modal__open')
+})
+
+closeButton.addEventListener('click', function() {
+  modalContact.classList.remove('modal__open')
+})
+
+modalContact.addEventListener('click', function(e) {
+  if (e.target.id == "contact-us") {
+    modalContact.classList.remove('modal__open')
+  }
 })
 
 
 // animation for input label
 inputs.forEach(function(input) {
   input.addEventListener('focus', function() {
-    var label = this.parentElement.querySelector('label')
+    var label = getFormLabel(input)
     label.classList.add("active")
     this.classList.add("active")
     this.setAttribute("data-placeholder",this.getAttribute('placeholder'))
@@ -23,7 +48,7 @@ inputs.forEach(function(input) {
 
 inputs.forEach(function(input) {
   input.addEventListener('blur', function() {
-    var label = this.parentElement.querySelector('label')
+    var label = getFormLabel(input)
     if(label.classList.contains('active') && !this.value) {
       label.classList.remove('active')
       this.classList.remove('active')
